616547d12fcb661b084558cc4d7e73457cf3f2a8
Balakumar1995 <sbalakumarece@gmail.com>
2016-06-07 11:39:15 +0530

    Update new.c

--- new.c
+++ new.c
@@ -1,30 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int cumfun(const void * a, const void * b)
+#include <stdio.h>
+int cmp(const void *x,const void *y)
 {
-   return ( *(int*)a - *(int*)b );
+    return (*(int*)x)-(*(int*)y);
 }
-
-main()
+int main()
 {
-int arr[10001],number,i,k,l,fg=0;
-char str[10001];
-scanf("%s",str);
-scanf("%d",&k);
-l=strlen(str);
-for(i=0;i<l;i++)
-	arr[i]=(int)str[i]%48;
-qsort(arr,l,sizeof(long long int), cumfun);
-for(i=0;i<l-k;i++)
-{
-if(arr[i]!=0){
-printf("%d",arr[i]);
-fg=1;}
-if(arr[i]==0&&fg==0)
-	fg=0;
-}
-if(fg==0)
-printf("0");
-return 0;
-}
+    int k,l,b[10001],i;
+    char a[10001];
+    scanf("%s %d",a,&k);
+    l=strlen(a);
+    for(i=0;i<l;i++)
+    {
+        b[i]=a[i]-48;
+    }
+    qsort(b,l,sizeof(b[0]),cmp);
+    for(i=0;i<l-k;i++)
+    {
+        printf("%d",b[i]);
+    }
+    return 0;
